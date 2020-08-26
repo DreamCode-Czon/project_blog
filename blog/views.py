7936bceef9b0786d0ab8718dd4310dd8907cc770
np03a170120 <np03a170120@heraldcollege.edu.np>
2020-08-26 12:33:47 +0545

    done database & migrations

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,16 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        "author": "dreamCode",
-        "title": "dream Big",
-        "content": "dream True",
-        "date_posted": "Sep 16,2020",
-    },
-    {
-        "author": "dreamCode",
-        "title": "dream Big",
-        "content": "dream True",
-        "date_posted": "Sep 16,2020",
-    },
-]
+from blog.models import Post
 
 
 def home(request):
-    context = {"posts": posts}
+    context = {
+        "posts": Post.objects.all(),
+    }
 
     return render(request, "blog/index.html", context)
 
 
 def about(request):
 
-    context = {"variable": posts}
+    return render(request, "blog/about.html", {"title": "about"})
 
-    return render(request, "blog/about.html", context)
