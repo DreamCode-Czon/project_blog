eccf32062b6aa3379d82df80e3e06b6fe3dcf6dd
np03a170120 <np03a170120@heraldcollege.edu.np>
2020-09-03 11:13:20 +0545

    REVIEW

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from blog.models import Post
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+)
 
 
-def home(request):
-    context = {
-        "posts": Post.objects.all(),
-    }
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
 
-    return render(request, "blog/index.html", context)
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
